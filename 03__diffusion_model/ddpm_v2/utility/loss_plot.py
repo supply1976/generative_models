dc6fd99a7075679b434e4193d1fa633f808ed31c
Rich WU <richwu@synopsys.com>
2025-02-24 17:35:50 +0800

    add more

--- 03__diffusion_model/ddpm_v2/utility/loss_plot.py
+++ 03__diffusion_model/ddpm_v2/utility/loss_plot.py
@@ -4,14 +4,23 @@ import numpy as np
 import matplotlib as mpl
 mpl.rcParams['backend']='TkAgg'
 import matplotlib.pyplot as plt
-plt.rcParams.update({'font.size': 20})
+#plt.rcParams.update({'font.size': 12})
 import yaml
 
+
+def exp_moving_avg(data, ema=0.999):
+  ema_data = np.zeros_like(data)
+  ema_data[0] = data[0]
+  for i in range(1, len(data)):
+    ema_data[i] = ema * ema_data[i-1] + (1-ema) * data[i]
+  return ema_data
   
+
+
 csv_files = sys.argv[1:]
 
 yaml_fn = "training_config.yaml"
-fig, ax = plt.subplots(figsize=(12,9))
+fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(15,6), sharey=True)
 
 for csv in csv_files:
   if yaml_fn in os.listdir(os.path.dirname(csv)):
@@ -29,9 +38,22 @@ for csv in csv_files:
     label = None 
   
   dfloss = pd.read_csv(csv, sep=",")
-  dfloss.plot(
-    x='epoch', y='loss', 
-    logy=True, ax=ax, grid=True, label=label, alpha=0.7)
+  dfloss['ema999_loss'] = exp_moving_avg(dfloss.loss.values, ema=0.999)
+  dfloss['ema99_loss'] = exp_moving_avg(dfloss.loss.values, ema=0.99)
 
-ax.legend()
+  dfloss.plot(
+    x='epoch', y='loss', logy=True, ax=axes[0], grid=True, alpha=0.7)
+  dfloss.plot(
+    x='epoch', y='ema999_loss', logy=True, ax=axes[1], grid=True, alpha=0.7)
+  dfloss.plot(
+    x='epoch', y='ema99_loss', 
+    label=label, logy=True, ax=axes[2], grid=True, alpha=0.7)
+
+
+axes[0].get_legend().remove()
+axes[1].get_legend().remove()
+axes[2].legend()
+
+axes[1].set_title("ema=0.999")
+axes[2].set_title("ema=0.99")
 plt.show()
