d525cb468f59ad35d03a002360090f1bf0b1cf45
Rich WU <richwu@synopsys.com>
2025-07-19 10:42:12 +0800

    update utilit for display generated images

--- 03__diffusion_model/ddpm_v3/utility/show_images.py
+++ 03__diffusion_model/ddpm_v3/utility/show_images.py
@@ -5,6 +5,7 @@ import matplotlib as mpl
 mpl.rcParams['backend']='TkAgg'
 import matplotlib.pyplot as plt
 import argparse
+#from skimage.transform import resize
 
 
 def _get_img_grad_hist(images, bins):
@@ -36,10 +37,13 @@ def display_images(images, nrows, ncols):
   figs, img_axess, gradx_axess, grady_axess = ([], [], [], [])
   for n in range(num_figs):
     if n>=4: break
-    fig1, img_axes = plt.subplots(nrows, ncols, sharex=True, sharey=True, figsize=(14,8))
+    fig1, img_axes = plt.subplots(nrows, ncols, sharex=True, sharey=True,figsize=(8,6))
     #fig2, gradx_axes = plt.subplots(nrows, ncols, sharex=True, sharey=True, figsize=(14,8))
     #fig3, grady_axes = plt.subplots(nrows, ncols, sharex=True, sharey=True, figsize=(14,8))
-    img_axes = img_axes.flatten()
+    if nrows*ncols > 1:
+      img_axes = img_axes.flatten()
+    else:
+      img_axes = [img_axes]
     #gradx_axes = gradx_axes.flatten()
     #grady_axes = grady_axes.flatten()
     img_axess.extend(img_axes)
@@ -49,7 +53,12 @@ def display_images(images, nrows, ncols):
 
   for i, img in enumerate(images):
     img_axess[i].imshow(img, cmap='gray')
-    img_axess[i].set_axis_off()
+    if c==2 or c==3:
+      img_axess[i].contour(img[:,:,0], levels=[0.5], colors='red')
+      img_axess[i].contour(img[:,:,1], levels=[0.5], colors='blue')
+    if c==3:
+      img_axess[i].contour(img[:,:,2], levels=[0.5], colors='green')
+    #img_axess[i].set_axis_off()
 
     #gradx_axess[i].plot(pdf_fx[i], '-o')
     
@@ -63,6 +72,7 @@ def display_images(images, nrows, ncols):
 
 def main():
   parser = argparse.ArgumentParser()
+  parser.add_argument('ij', nargs=2)
   parser.add_argument('--npz', type=str, default=None)
   parser.add_argument('--feats_csv', type=str, default=None)
   FLAGS, _ = parser.parse_known_args()
@@ -81,7 +91,10 @@ def main():
     feats = np.around(df[['Hx','Hy']].values, 2)
     print(feats.shape)
 
-  display_images(images, 2, 2)
+  i, j = list(map(int, FLAGS.ij))
+
+  #display_images(images_resized, 15, 20)
+  display_images(images, i, j)
   
   #images_t = data['images_t_300']
   #display_images(images_t, 4, 5)
